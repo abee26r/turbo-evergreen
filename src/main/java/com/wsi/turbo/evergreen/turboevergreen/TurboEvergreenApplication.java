5ef2c41ed74779ba48ad3887dcbfdf83f2c7c3b6
abee26r <ab.almighty@gmail.com>
2017-11-16 22:47:40 -0800

    init repo

---
+++ src/main/java/com/wsi/turbo/evergreen/turboevergreen/TurboEvergreenApplication.java
@@ -0,0 +1,12 @@
+package com.wsi.turbo.evergreen.turboevergreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboEvergreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboEvergreenApplication.class, args);
+	}
+}
